0218a484a844d882a46798e3cdb772be4c634d3d
Hentai-Princess <Hentai-Princess@users.noreply.github.com>
2017-10-18 00:35:09 +0300

    Initial Commit

---
+++ Dijkstra/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Dijkstra
+//
+//  Created by Hentai Princess on 18.10.17.
+//  Copyright © 2017 Hentai Princess. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
